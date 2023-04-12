f59017be514f98d0bda57ee196965b3fe3aadc6d
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-04-12 15:59:52 +0300

    hw03

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://yana:1602@cluster0.fmvxyx6.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => console.log(error.message));
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
+  process.exit(1);
 });
